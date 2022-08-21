a3da1bf95b668edc625b8fd813c8279be6e5a2be
cburgyan <cburgyan@gmail.com>
2022-08-21 08:49:23 -0500

    add functionality to login and with it, a requirement to be logged in to see secrets page

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, request, url_for, redirect, flash, sen
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import time
+
 
 app = Flask(__name__)
 
@@ -9,20 +14,32 @@ app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 # #CREATE TABLE IN DB
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    salt = db.Column(db.String(100))
-    hash_method = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(1000), nullable=False)
+    is_authenticated = db.Column(db.Boolean, nullable=False)
+    is_active = db.Column(db.Boolean, nullable=False)
+    is_anonymous = db.Column(db.Boolean, nullable=False)
+
+    def get_id(self):
+        return str(self.id)
 
 
 # Line below only required once, when creating DB.
 db.create_all()
+
+
+# class LoginForm(FlaskForm):
+#     user = StringField('Email: ', validators=[DataRequired()])
+#     password = StringField('Password: ', validators=[DataRequired()])
+#     submit = SubmitField('Let Me In, Please')
 
 
 @app.route('/')
@@ -38,33 +55,64 @@ def register():
         password = request.form.get('password')
         generated_hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
         print(generated_hash)
-        generated_hash_list = generated_hash.split('$')
 
-        new_user = User(name=name, email=email, password=generated_hash_list[2], salt=generated_hash_list[1], hash_method=generated_hash_list[0])
+        # One should really get authenticated by email verification but that implementation will have to come later
+        new_user = User(name=name, email=email, password=generated_hash, is_authenticated=True, is_active=True, is_anonymous=False)
         db.session.add(new_user)
         db.session.commit()
 
-        return redirect(url_for('secrets'))
+        login_user(new_user)
+
+        return redirect(url_for('secrets', name=name))
 
     return render_template("register.html")
 
 
-@app.route('/login')
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter_by(id=user_id).first()
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    # form = LoginForm()
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            # user.is_authenticated = True
+            # db.session.commit()
+            if check_password_hash(user.password, password):
+                login_user(user)
+                db.session.commit()
+                flash('Logged in successfully.')
+                print('Logged in successfully.')
+                return redirect(url_for('secrets', name=user.name))
+            else:
+                print('Login failed. Password failed.')
+        else:
+            flash('Login failed.')
+            print('Login failed. Failed to find user in database.')
+            time.sleep(2)
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    return render_template("secrets.html", name=request.args.get('name'))
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/download')
+@login_required
 def download():
     directory = './static/files'
     path = 'cheat_sheet.pdf'
